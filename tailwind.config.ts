ce15cc2eab7b68cd889338b1165e7408e501accf
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-12-26 13:09:31 +0000

    Add dark mode config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import forms from "@tailwindcss/forms";
 
 export default {
+  darkMode: "media",
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
