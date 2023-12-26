b719a92899f7b51de7518b0c37110ee07e066e0f
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-12-26 12:56:40 +0000

    Add Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms'
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms],
+} satisfies Config
+
