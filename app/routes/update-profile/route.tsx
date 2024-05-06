65a5a52560fec9c3d868027a580548bf2dc39a0e
Rahul Parkar <rahul.a.parkar@gmail.com>
2024-05-06 18:59:30 +0100

    Make Update Profile work

--- app/routes/update-profile/route.tsx
+++ app/routes/update-profile/route.tsx
@@ -1,18 +1,40 @@
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { authenticator } from "~/utils/auth.server";
+
 export default function UpdateProfile() {
   return (
     <div>
       <h1>Update Profile</h1>
-      <form>
+      <form method="post">
         <label>
-          Name
-          <input type="text" />
+          First Name
+          <input name="firstName" type="text" />
         </label>
         <label>
-          Email
-          <input type="email" />
+          Last Name
+          <input name="lastName" type="text" />
         </label>
-        <button type="submit">Update</button>
+        <button type="submit">Complete Profile</button>
       </form>
     </div>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  console.log("action");
+  const token = await authenticator.isAuthenticated(request);
+  const formData = await request.formData();
+
+  await fetch("http://localhost:4000/dev/auth/update-profile", {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+    }),
+  });
+
+  return json({ ok: true });
 }
